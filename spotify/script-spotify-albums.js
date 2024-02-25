210301907c3b00cb8a704e34b6f0fbb50573760d
Wesley Santos <paperrings273@gmail.com>
2024-02-25 18:34:29 -0300

    spotify charts 17-23

--- spotify/script-spotify-albums.js
+++ spotify/script-spotify-albums.js
@@ -1,22 +1,38 @@
 var tabelaDados = [
   {
     posicao: 1,
-    musica: "KIDS SEE GHOSTS",
-    artista: "Trevor",
+    musica: "House Of The Rising Sun",
+    artista: "Ava Thompson",
     imagem:
-      "https://i.imgur.com/uWggx9r.png"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1210048161218039818/image.png?ex=65e9242b&is=65d6af2b&hm=983d20efc5332a9b8319550d5630851253d8eb4a622fe9487f72397354bf3d0e&"
   },
 
   {
     posicao: 2,
-    musica: "Everything Is Freedom",
-    artista: "Trevor",
+    musica: "Everlasting",
+    artista: "Liz",
     imagem:
-      "https://i.imgur.com/3RlKr4c.jpg"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1208443813907927070/CAPA.png?ex=65ec8881&is=65da1381&hm=6259940612eaf70c7ea83b0cee9394dd8b48861a0befb41bff2184249bf97186&"
   },
 
   {
     posicao: 3,
+    musica: "I Owe It To Myself",
+    artista: "Owen Graham",
+    imagem:
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
+  },
+
+  {
+    posicao: 4,
+    musica: "KIDS SEE GHOSTS",
+    artista: "Trevor",
+    imagem:
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1208475894344060928/coverksg.png?ex=65eca661&is=65da3161&hm=8634d26183136228e3b4f1f8f17ef566d53d992ad284182e0e76c73d64ae33b5&"
+  },
+
+  {
+    posicao: 5,
     musica: "The Life Of Greg",
     artista: "Trevor",
     imagem:
@@ -24,50 +40,34 @@ var tabelaDados = [
   },
 
   {
-    posicao: 4,
-    musica: "Pure Feeling",
-    artista: "Liza",
-    imagem:
-      "https://i.imgur.com/jXLvpzn.jpg"
-  },
-
-  {
-    posicao: 5,
-    musica: "I owe it to myself",
-    artista: "Owen Graham",
-    imagem:
-      "https://i.imgur.com/VSfImF5.png"
-  },
-
-  {
     posicao: 6,
-    musica: "Love Signal",
-    artista: "Zion",
+    musica: "Everything Is Freedom",
+    artista: "Trevor",
     imagem:
-      "https://i.imgur.com/cYWu7Yx.jpg"
+      "https://i.imgur.com/3RlKr4c.jpg"
   },
 
   {
     posicao: 7,
-    musica: "When I Get Lost",
-    artista: "Conan Woolridge",
+      musica: "Pure Feeling",
+    artista: "Liza",
     imagem:
-      "https://i.imgur.com/J9FPzWb.png"
+    "https://i.imgur.com/jXLvpzn.jpg"
   },
 
   {
     posicao: 8,
+      musica: "When I Get Lost",
+    artista: "Conan Woolridge",
+    imagem:
+    "https://i.imgur.com/J9FPzWb.png"
+  },
+
+  { posicao: 9,
     musica: "Lockdown",
     artista: "Liz",
     imagem:
       "https://i.imgur.com/pEys1o7.png"
-  },
-
-  { posicao: 9,
-    musica: "VANGUARD",
-    artista: "Hannah",
-    imagem:
-    "https://i.imgur.com/kh25Tn7.png"
   },
 
   {
--- spotify/script-spotify-singles.js
+++ spotify/script-spotify-singles.js
@@ -1,85 +1,79 @@
 // Variáveis para armazenar os dados da tabela
 var tabelaDados = [
-  { posicao: 1,
-    musica: "Inner Self",
-    artista: "Trevor",
+  {
+    posicao: 1,
+    musica: "Here's For The Nights",
+    artista: "Liz",
     imagem:
-    "https://i.imgur.com/uWggx9r.png"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1208443813907927070/CAPA.png?ex=65ec8881&is=65da1381&hm=6259940612eaf70c7ea83b0cee9394dd8b48861a0befb41bff2184249bf97186&"
   },
 
   { posicao: 2,
-    musica: "Party Favors",
-    artista: "Zara",
+    musica: "Feeling",
+    artista: "Ava Thompson",
     imagem:
-      "https://i.imgur.com/FgZ69Ya.jpeg"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1210048161218039818/image.png?ex=65e9242b&is=65d6af2b&hm=983d20efc5332a9b8319550d5630851253d8eb4a622fe9487f72397354bf3d0e&"
   },
 
   { posicao: 3,
-    musica: "Blame It On Me",
-    artista: "Trevor",
+    musica: "I Didn't Know Any...",
+    artista: "Owen Graham",
     imagem:
-    "https://i.imgur.com/uWggx9r.png"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
   },
 
   { posicao: 4,
-    musica: "EARTHQUAKE",
-    artista: "Trevor",
+    musica: "Second",
+    artista: "Owen Graham",
     imagem:
-    "https://i.imgur.com/WPYcao8.png"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
   },
 
   {
     posicao: 5,
-    musica: "ALRIGHT",
-    artista: "Trevor",
+    musica: "In The Middle",
+    artista: "Owen Graham",
     imagem:
-      "https://i.imgur.com/3RlKr4c.jpg"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
   },
 
   {
     posicao: 6,
-    musica: "I HAVE A DREAM",
-    artista: "Trevor",
+    musica: "The Very Modern Dance",
+    artista: "Even Grace",
     imagem:
-      "https://i.imgur.com/3RlKr4c.jpg"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208523242478903296/capa1-1.png?ex=65ecd27a&is=65da5d7a&hm=55bfaa29c47ce69d91274079a5776a408638a37cf0e445a6165be03902c6a631&",
   },
 
   {
     posicao: 7,
-    musica: "CAN WE BE FRIENDS?",
-    artista: "Trevor",
+    musica: "Sugarcane",
+    artista: "Owen Graham ft. Bianca",
     imagem:
-      "https://i.imgur.com/WPYcao8.png"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
   },
 
   {
     posicao: 8,
-    musica: "feel like sheet",
-    artista: "Ivy Sinclair",
+    musica: "Inner Self",
+    artista: "Trevor",
     imagem:
-      "https://i.imgur.com/SlcGzdw.png"
+      "https://i.imgur.com/uWggx9r.png",
   },
 
   {
     posicao: 9,
-    musica: "Beetlebum",
-    artista: "Even Grace",
+    musica: "In The Garden",
+    artista: "Owen Graham",
     imagem:
-      "https://i.imgur.com/eTjuhMK.png"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
   },
 
   { posicao: 10,
-    musica: "Fantasmagoric Feelings",
-    artista: "Liza",
+    musica: "I Walked A Mile In My...",
+    artista: "Owen Graham",
     imagem:
-    "https://i.imgur.com/jXLvpzn.jpg"
-  },
-
-  { posicao: 11,
-    musica: "Juju do pix",
-    artista: "me dá pix",
-    imagem:
-      "https://s2-techtudo.glbimg.com/DltGJnURD11lOxdsACc34epHtn0=/400x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/r/i/lPgLATQ7iBy4bu9wX5VQ/lanaexplicit-gretchen-meme-eu-teria-vergonha-disso-.jpg"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
   },
 ];
 
