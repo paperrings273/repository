a72b4edfc5a9745954b71d99c01e57d3061aab24
Wesley Santos <paperrings273@gmail.com>
2024-02-26 02:52:26 -0300

    charts billboard dia 25 de fevereiro

--- spotify/script-billboard200.js
+++ spotify/script-billboard200.js
@@ -1,14 +1,14 @@
 var tabelaDados = [
-  { posicao: 1, album: "KIDS SEE GHOSTS", artista: "Trevor" },
-  { posicao: 2, album: "Everything Is Freedom", artista: "Trevor" },
-  { posicao: 3, album: "The Life Of Greg", artista: "Trevor" },
+  { posicao: 1, album: "House Of The Rising Sun", artista: "Ava Thompson" },
+  { posicao: 2, album: "Everlasting", artista: "Liz" },
+  { posicao: 3, album: "KIDS SEE GHOSTS", artista: "Trevor" },
   { posicao: 4, album: "I owe it to myself", artista: "Owen Graham" },
-  { posicao: 5, album: "Purple Dream", artista: "Even Grace" },
-  { posicao: 6, album: "Pure Feeling", artista: "Liza" },
-  { posicao: 7, album: "Love Signal", artista: "Zion" },
+  { posicao: 5, album: "Everything Is Freedom", artista: "Trevor" },
+  { posicao: 6, album: "The Life Of Greg", artista: "Trevor" },
+  { posicao: 7, album: "Purple Dream", artista: "Even Grace" },
   { posicao: 8, album: "When I Get Lost", artista: "Conan Woolridge" },
   { posicao: 9, album: "Lockdown", artista: "Liz" },
-  { posicao: 10, album: "VANGUARD", artista: "Hannah" },
+  { posicao: 10, album: "Love Signal", artista: "Zion" },
 ];
 
 function atualizarTabela() {
--- spotify/script-hot100.js
+++ spotify/script-hot100.js
@@ -1,14 +1,14 @@
 var tabelaDados = [
-  { posicao: 1, musica: "Party Favors", artista: "Zara", imagem: "https://i.imgur.com/FgZ69Ya.jpg" },
-  { posicao: 2, musica: "Inner Self", artista: "Trevor", imagem: "" },
-  { posicao: 3, musica: "Blame It On Me", artista: "Trevor", imagem: "" },
-  { posicao: 4, musica: "I Didn't Know Any Better", artista: "Owen Graham", imagem: "" },
-  { posicao: 5, musica: "Timeless", artista: "Owen Graham", imagem: "" },
-  { posicao: 6, musica: "Second", artista: "Owen Graham", imagem: "" },
-  { posicao: 7, musica: "Feel Like Sheet", artista: "Ivy Sinclair", imagem: "" },
-  { posicao: 8, musica: "Beetlebum", artista: "Even Grace", imagem: "" },
-  { posicao: 9, musica: "In The Middle", artista: "Owen Graham", imagem: "" },
-  { posicao: 10, musica: "Nightmare", artista: "Trevor", imagem: "" },
+  { posicao: 1, musica: "Feeling", artista: "Ava Thompson", imagem: "https://i.imgur.com/FgZ69Ya.jpg" },
+  { posicao: 2, musica: "Here's To The Nights", artista: "Liz", imagem: "" },
+  { posicao: 3, musica: "I Didn't Know Any Better", artista: "Owen Graham", imagem: "" },
+  { posicao: 4, musica: "Timeless", artista: "Owen Graham", imagem: "" },
+  { posicao: 5, musica: "The Very Modern Dance", artista: "Even Grace", imagem: "" },
+  { posicao: 6, musica: "Sugarcane", artista: "Owen Graham ft. Bianca", imagem: "" },
+  { posicao: 7, musica: "Second", artista: "Owen Graham", imagem: "" },
+  { posicao: 8, musica: "The Garden", artista: "Owen Graham", imagem: "" },
+  { posicao: 9, musica: "I Walked I Mile In My Room", artista: "Owen Graham", imagem: "" },
+  { posicao: 10, musica: "(I Hate Myself Sometimes)", artista: "Owen Graham", imagem: "" },
 ];
 
 function atualizarTabela() {
