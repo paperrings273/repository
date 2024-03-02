7800dd02f076c3d0faf379bea88e56370f0abbbf
Wesley Santos <paperrings273@gmail.com>
2024-03-02 15:15:10 -0300

    spotify charts 3

--- spotify/script-spotify-albums.js
+++ spotify/script-spotify-albums.js
@@ -4,23 +4,23 @@ var tabelaDados = [
     musica: "House Of The Rising Sun",
     artista: "Ava Thompson",
     imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1210048161218039818/image.png?ex=65e9242b&is=65d6af2b&hm=983d20efc5332a9b8319550d5630851253d8eb4a622fe9487f72397354bf3d0e&"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1210048161218039818/image.png?ex=65f25eab&is=65dfe9ab&hm=f89e9315cb30b153a51fd41d29d4d3cad9bc16f93dff1f1e968ca4a0c13ecfb4&"
   },
 
   {
     posicao: 2,
-    musica: "Everlasting",
+    musica: "everlasting",
     artista: "Liz",
     imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1208443813907927070/CAPA.png?ex=65ec8881&is=65da1381&hm=6259940612eaf70c7ea83b0cee9394dd8b48861a0befb41bff2184249bf97186&"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208443813907927070/CAPA.png?ex=65ec8881&is=65da1381&hm=6259940612eaf70c7ea83b0cee9394dd8b48861a0befb41bff2184249bf97186&"
   },
 
   {
     posicao: 3,
-    musica: "I Owe It To Myself",
+    musica: "i owe it to myself",
     artista: "Owen Graham",
     imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65f06dac&is=65ddf8ac&hm=705ec699cc443b166ba4ac5bf010a9ea8b36be604bbd984b29d96b6bc83d8c19&"
   },
 
   {
@@ -28,54 +28,54 @@ var tabelaDados = [
     musica: "KIDS SEE GHOSTS",
     artista: "Trevor",
     imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1208475894344060928/coverksg.png?ex=65eca661&is=65da3161&hm=8634d26183136228e3b4f1f8f17ef566d53d992ad284182e0e76c73d64ae33b5&"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1205585992355156028/coverksg.png?ex=65f497f3&is=65e222f3&hm=dcfa0090f3fd497dccb47363657b34ea3a5b7992b5900f79515045dbe6c1a99a&"
   },
 
   {
     posicao: 5,
-    musica: "The Life Of Greg",
-    artista: "Trevor",
+    musica: "When I Get Lost",
+    artista: "Conan Woolridge",
     imagem:
-      "https://i.imgur.com/WPYcao8.png"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208454061439844352/whenigetlost.png?ex=65ec920c&is=65da1d0c&hm=be9c64a73febcb0634ef6704b172b488fcb4b0db4a32011aab112666673f8db1&"
   },
 
   {
     posicao: 6,
-    musica: "Everything Is Freedom",
-    artista: "Trevor",
+    musica: "Purple Dream",
+    artista: "Even Grace",
     imagem:
-      "https://i.imgur.com/3RlKr4c.jpg"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208523242478903296/capa1-1.png?ex=65ecd27a&is=65da5d7a&hm=55bfaa29c47ce69d91274079a5776a408638a37cf0e445a6165be03902c6a631&"
   },
 
   {
     posicao: 7,
-      musica: "Pure Feeling",
-    artista: "Liza",
+    musica: "EVERYTHING IS FREEDOM",
+    artista: "Trevor",
     imagem:
-    "https://i.imgur.com/jXLvpzn.jpg"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208475765801353286/eif.png?ex=65eca642&is=65da3142&hm=b277c14455e659fe2fc7bdf941315e23ee9943e106f3685857f3216d7103e343&"
   },
 
   {
     posicao: 8,
-      musica: "When I Get Lost",
-    artista: "Conan Woolridge",
+    musica: "Confessions Of A Broken Heart (Deluxe)",
+    artista: "Fiona",
     imagem:
-    "https://i.imgur.com/J9FPzWb.png"
-  },
-
-  { posicao: 9,
-    musica: "Lockdown",
-    artista: "Liz",
-    imagem:
-      "https://i.imgur.com/pEys1o7.png"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208581396092817528/20240125_235905.jpg?ex=65ed08a3&is=65da93a3&hm=64f4ff4031466b5d67f5f17fe432c76f15c68ae08fb690df524719ebad44e781&"
   },
 
   {
-    posicao: 10,
-    musica: "Purple Dream",
-    artista: "Even Grace",
+    posicao: 9,
+    musica: "The Life Of Greg",
+    artista: "Trevor",
     imagem:
-      "https://i.imgur.com/pXBYVJp.png"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208473057090478090/tlog.png?ex=65eca3bd&is=65da2ebd&hm=3874fa1e2b96027a903887d3de7613de9adf99daf93b1e624e07f0684e2a5385&"
+  },
+
+  { posicao: 10,
+    musica: "Pure Feeling",
+    artista: "Liza",
+    imagem:
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208497806185074818/jXLvpzn.png?ex=65ecbac9&is=65da45c9&hm=c3d56f82ca4788ad5afb8958a266c7bdc78690c6b7648c876bba7e1dc93d2b76&"
   },
 ];
 
--- spotify/script-spotify-singles.js
+++ spotify/script-spotify-singles.js
@@ -2,78 +2,80 @@
 var tabelaDados = [
   {
     posicao: 1,
-    musica: "Here's To The Nights",
-    artista: "Liz",
-    imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1208443813907927070/CAPA.png?ex=65ec8881&is=65da1381&hm=6259940612eaf70c7ea83b0cee9394dd8b48861a0befb41bff2184249bf97186&"
-  },
-
-  { posicao: 2,
-    musica: "Feeling",
+    musica: "Soul For My Love",
     artista: "Ava Thompson",
     imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1210048161218039818/image.png?ex=65e9242b&is=65d6af2b&hm=983d20efc5332a9b8319550d5630851253d8eb4a622fe9487f72397354bf3d0e&"
+      "https://cdn.discordapp.com/attachments/1200979781286625300/1213365437723574342/image.png?ex=65f5359f&is=65e2c09f&hm=b9b2510b3b8403788ad9653e06a712d85ccf7f7e76e9f2144f84a2418e0fbfbd&"
   },
 
-  { posicao: 3,
-    musica: "I Didn't Know Any...",
-    artista: "Owen Graham",
+  {
+    posicao: 2,
+    musica: "Pleasure Addiction",
+    artista: "Aaron",
     imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
+      "https://cdn.discordapp.com/attachments/1200979781286625300/1211845091707195472/Novo_Projeto_22.png?ex=65efadb0&is=65dd38b0&hm=e9c9274e72590d924ec1d10d75e742c5345b0fd379915bb6c9802c083c1d49b4&"
+  },
+
+  {
+    posicao: 3,
+    musica: "Beetlebum",
+    artista: "Even Grace",
+    imagem:
+      "https://cdn.discordapp.com/attachments/1200979781286625300/1212962327427686431/single1.png?ex=65f3be32&is=65e14932&hm=7aa7924c87fdf88777ff1c144b12af521df0ea17b719ba94b9da20314281e907&"
   },
 
   { posicao: 4,
-    musica: "Second",
-    artista: "Owen Graham",
+    musica: "20",
+    artista: "Conan Woolridge",
     imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
+      "https://cdn.discordapp.com/attachments/1200979781286625300/1212962238038679582/20cover.png?ex=65f3be1d&is=65e1491d&hm=ae306d5a8d9835ee6380e47e2676ce3b70444c14ae9de78d482f0e35bb8601b6&"
   },
 
   {
     posicao: 5,
-    musica: "In The Middle",
+    musica: "grocery story",
     artista: "Owen Graham",
     imagem:
-      "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
+    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65f06dac&is=65ddf8ac&hm=705ec699cc443b166ba4ac5bf010a9ea8b36be604bbd984b29d96b6bc83d8c19&"
   },
 
   {
     posicao: 6,
-    musica: "The Very Modern Dance",
-    artista: "Even Grace",
+    musica: "Here's To The Nights",
+    artista: "Liz",
     imagem:
-      "https://cdn.discordapp.com/attachments/1200979746297749656/1208523242478903296/capa1-1.png?ex=65ecd27a&is=65da5d7a&hm=55bfaa29c47ce69d91274079a5776a408638a37cf0e445a6165be03902c6a631&",
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208443813907927070/CAPA.png?ex=65ec8881&is=65da1381&hm=6259940612eaf70c7ea83b0cee9394dd8b48861a0befb41bff2184249bf97186&"
   },
 
   {
     posicao: 7,
-    musica: "Sugarcane",
-    artista: "Owen Graham ft. Bianca",
+    musica: "Trevor",
+    artista: "Inner Self",
     imagem:
-      "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
+      "https://cdn.discordapp.com/attachments/1200979746297749656/1208475894344060928/coverksg.png?ex=65eca661&is=65da3161&hm=8634d26183136228e3b4f1f8f17ef566d53d992ad284182e0e76c73d64ae33b5&"
   },
 
   {
     posicao: 8,
-    musica: "Inner Self",
-    artista: "Trevor",
+    musica: "Party Favors",
+    artista: "Zara",
     imagem:
-      "https://i.imgur.com/uWggx9r.png",
+      "https://cdn.discordapp.com/attachments/1200979781286625300/1207400286465822720/capa.jpg?ex=65f1f724&is=65df8224&hm=fec52eaa6c08cd43ae573caa772f28d2551e3a8afe43026e9d08820d42533d7a&"
   },
 
   {
     posicao: 9,
-    musica: "The Garden",
-    artista: "Owen Graham",
+    musica: "You Broke Me First",
+    artista: "Ivy Sinclair",
     imagem:
-      "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
+      "https://cdn.discordapp.com/attachments/1200979781286625300/1205274467492696114/Purple_And_Red_Minimalist_CD_Cover.png?ex=65f375d1&is=65e100d1&hm=993ee7a31eb30e29e290cf1a9eab7e95e68f644d5ff098af21a14ede15b3b7ef&"
   },
 
   { posicao: 10,
-    musica: "I Walked A Mile In My...",
-    artista: "Owen Graham",
+    musica: "Lunatic",
+    artista: "Anthony",
     imagem:
-    "https://cdn.discordapp.com/attachments/1200979746297749656/1204441086244691968/owen-capa-1.png?ex=65e7332c&is=65d4be2c&hm=f3b3266f900b3bba9c365aa0b62d3b9ac78938e0d076c041dd52e2d0f9c357a0&"
+      "https://cdn.discordapp.com/attachments/982022570469646366/1213518840009334885/like_a_killer.png?ex=65f5c47d&is=65e34f7d&hm=20b4e04a73c1be3ae276f5a2920cfdc4e1c87e63088b474c6b685bf604f47823&"
   },
 ];
 
@@ -91,5 +93,4 @@ function atualizarTabela() {
   });
 }
 
-// Chama a função para atualizar a tabela
 atualizarTabela();
