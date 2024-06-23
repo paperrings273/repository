c74b1d616fe1eeeba3546d42320712de48eda50a
Wesley Santos <paperrings273@gmail.com>
2024-06-23 18:32:59 -0300

    Novos artistas

---
+++ artistas/anthony/discography.js
@@ -0,0 +1,20 @@
+const albumsData = [
+  { title: "meaningless",
+    year: "Year 01",
+    image: "https://i.imgur.com/H2zKlDG.png",
+    label: "℗ © Illuminati Records"
+  }
+];
+
+const singlesData = [
+  { title: "Blind",
+    year: "Year 01",
+    image: "https://i.imgur.com/q9Qqizb.png",
+    label: "℗ © Illuminati Records"
+  },
+  { title: "lunatic",
+    year: "Year 01",
+    image: "https://i.imgur.com/SpxCMFk.png",
+    label: "℗ © Illuminati Records"
+  },
+];
