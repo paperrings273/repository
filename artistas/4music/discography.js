7684a93f4a6c070194e8a3d060f784e2fb12367a
Wesley Santos <paperrings273@gmail.com>
2024-06-23 20:31:07 -0300

    new artists

---
+++ artistas/4music/discography.js
@@ -0,0 +1,16 @@
+const albumsData = [
+
+]
+
+const singlesData = [
+  { title: "Wings",
+    year: "Year 02",
+    image: "https://i.imgur.com/FFwDAJ2.png",
+    label: "℗ © Dawn Records"
+  },
+  { title: "Salute",
+    year: "Year 02",
+    image: "https://i.imgur.com/oxCDtRj.jpg",
+    label: "℗ © Dawn Records"
+  },
+];
---
+++ artistas/fiona/discography.js
@@ -0,0 +1,50 @@
+const albumsData = [
+  { title: "Confessions Of A Broken Heart (Deluxe)",
+    year: "Year 01",
+    image: "https://i.imgur.com/GhEMkMK.jpeg",
+    label: "℗ © Dawn Records"
+  },
+  { title: "Confessions Of A Broken Heart",
+    year: "-",
+    image: "https://i.imgur.com/dowVhSr.jpeg",
+    label: "℗ © Dawn Records"
+  },
+];
+
+const singlesData = [
+  { title: "When The Party's Over",
+    year: "Year 02",
+    image: "https://i.imgur.com/wN6WNhi.jpeg",
+    label: "℗ © Dawn Records"
+  },
+  { title: "My Game (Remix) feat. Ava",
+    year: "Year 01",
+    image: "https://i.imgur.com/hBMdtGF.jpeg",
+    label: "℗ © Dawn Records"
+  },
+  { title: "My Game",
+    year: "Year 01",
+    image: "https://i.imgur.com/dPHfzSg.jpeg",
+    label: "℗ © Dawn Records"
+  },
+  { title: "Reckless",
+    year: "-",
+    image: "https://i.imgur.com/RXgkKT4.jpeg",
+    label: "℗ © Dawn Records"
+  },
+  { title: "Confessions Of A Broken Heart",
+    year: "-",
+    image: "https://i.imgur.com/kvQV8Dg.jpeg",
+    label: "℗ © Dawn Records"
+  },
+  { title: "Hold Me Close",
+    year: "-",
+    image: "https://i.imgur.com/OteWWIH.jpeg",
+    label: "℗ © Dawn Records"
+  },
+  { title: "True Romance",
+    year: "-",
+    image: "https://i.imgur.com/q4UISeb.jpeg",
+    label: "℗ © Dawn Records"
+  },
+];
