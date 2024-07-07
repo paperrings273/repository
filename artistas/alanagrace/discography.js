656e46925889dc428102e65b12d7404d3a84d2d5
Wesley Santos <paperrings273@gmail.com>
2024-07-07 15:23:00 -0300

    atualizações

---
+++ artistas/alanagrace/discography.js
@@ -0,0 +1,10 @@
+const albumsData = [
+  { title: "MELANCHOLIA",
+    year: "Year 02",
+    image: "https://i.imgur.com/n5OTHPi.png",
+    label: "℗ © Dawn Records"
+  }
+];
+
+const singlesData = [
+];
